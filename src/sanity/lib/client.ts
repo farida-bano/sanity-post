bef66b992ebbd17d90d125b099742f43725b3a2c
farida <bano23086@gmaile.com>
2024-12-30 20:45:16 +0500

    first commit

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -2,8 +2,8 @@ import { createClient } from 'next-sanity';
 
 // Directly passing the environment variables into createClient
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,  // Accessing project ID from .env.local
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,      // Accessing dataset from .env.local
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION, // Accessing API version from .env.local
+  projectId: "fyv1no97",
+  dataset:"production",
+  apiVersion: "2023-10-30",
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 });
